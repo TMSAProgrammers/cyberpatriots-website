8506c438835bdae0fcecbe3eb6618f7587549439
Jason Cobb <jason.e.cobb@gmail.com>
2017-11-07 21:20:06 -0500

    Extract navbar code

---
+++ navbar_include.js
@@ -0,0 +1,24 @@
+// Include stylesheet
+document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"CSS/navbar.css\">");
+
+// Include navbar
+document.write(
+    "<!-- Navbar -->" +
+    "<div id=\"navbar\">" +
+        "<a href=\"index.html\">" +
+            "<button>Home</button>" +
+        "</a>" +
+        "<a href=\"calendar.html\">" +
+            "<button>Calendar</button>" +
+        "</a>" +
+        "<a href=\"members.html\">" +
+          "<button>Members</button>" +
+        "</a>" +
+        "<a href=\"resources.html\">" +
+          "<button>Resources</button>" +
+        "</a>" +
+        "<a>" +
+            "<button onclick=\"snackbar()\">Blog</button>" +
+        "</a>" +
+        "<div id=\"snackbar\">This has yet to be completed</div>" +
+    "</div>");
