5225c2072dfb3ce4097404a55b14824a5a2b7906
Jason Cobb <jason.e.cobb@gmail.com>
2017-12-23 15:23:16 -0500

    Fix up a few things Reformat code
    Signed-off-by: Jason Cobb <jason.e.cobb@gmail.com>

--- default_include.js
+++ default_include.js
@@ -5,24 +5,24 @@ document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"CSS/default.cs
 document.write(
     "<!-- Navbar -->" +
     "<div id=\"navbar\">" +
-        "<a href=\"home.html\">" +
-            "<button>Home</button>" +
-        "</a>" +
-        "<a href=\"calendar.html\">" +
-            "<button>Calendar</button>" +
-        "</a>" +
-        "<a href=\"members.html\">" +
-          "<button>Members</button>" +
-        "</a>" +
-        "<a href=\"resources.html\">" +
-          "<button>Resources</button>" +
-        "</a>" +
-        "<a>" +
-            "<button onclick=\"snackbar()\">Blog</button>" +
-        "</a>" +
-        "<div id=\"snackbar\">This has yet to be completed</div>" +
+    "<a href=\"home.html\">" +
+    "<button>Home</button>" +
+    "</a>" +
+    "<a href=\"calendar.html\">" +
+    "<button>Calendar</button>" +
+    "</a>" +
+    "<a href=\"members.html\">" +
+    "<button>Members</button>" +
+    "</a>" +
+    "<a href=\"resources.html\">" +
+    "<button>Resources</button>" +
+    "</a>" +
+    "<a>" +
+    "<button onclick=\"snackbar()\">Blog</button>" +
+    "</a>" +
+    "<div id=\"snackbar\">This has yet to be completed</div>" +
 
-		
+
     "</div>");
 
 function snackbar() {
@@ -33,5 +33,7 @@ function snackbar() {
     x.className = "show";
 
     // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    setTimeout(function () {
+        x.className = x.className.replace("show", "");
+    }, 3000);
 }
